11329d280426468cc95466cb8d70afd3155149af
Zaidurrohman <zaidurrohmaan@gmail.com>
2026-08-05 10:30:07 +0700

    add day 1 solution

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,20 +1,33 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val list = input.map { it.toInt() }.sorted()
+        var i = 0
+        var j = input.size - 1
+
+        while (i < j) {
+            val sum = list[i] + list[j]
+            if (sum == 2020) return list[i] * list[j]
+            if (sum < 2020) i++
+            else j--
+        }
+        return 0
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val list = input.map { it.toInt() }
+
+        for (i in 0 until list.size) {
+            for (j in i+1 until list.size) {
+                for (k in j+1 until list.size) {
+                    val sum = list[i] + list[j] + list[k]
+                    if (sum == 2020) return list[i] * list[j] * list[k]
+                }
+            }
+        }
+
+        return 0
     }
 
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
-
-    // Or read a large test input from the `src/Day01_test.txt` file:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
-    // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
     part1(input).println()
     part2(input).println()
