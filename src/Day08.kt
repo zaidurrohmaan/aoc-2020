6d165d57753442e5796141a0ae3314ea81e4f7bc
Zaidurrohman <zaidurrohmaan@gmail.com>
2026-08-05 21:01:02 +0700

    add faster solution for day 8

--- src/Day08.kt
+++ src/Day08.kt
@@ -62,7 +62,64 @@ fun main() {
         return 0
     }
 
+    fun part2Graph(input: List<String>): Int {
+        val visitors = MutableList(input.size) { mutableListOf<Int>() }
+
+        input.forEachIndexed { i, line ->
+            val dest = when (line.substringBefore(" ")) {
+                "nop" -> i+1
+                "jmp" -> i+line.substringAfter(" ").toInt()
+                else -> i+1
+            }
+
+            if (dest in visitors.indices) visitors[dest].add(i)
+        }
+
+        val correct = MutableList(input.size) { false }
+        correct[input.size-1] = true
+
+        val queue = ArrayDeque<Int>()
+        queue.add(input.size-1)
+
+        while (queue.isNotEmpty()) {
+            val idx = queue.removeFirst()
+            correct[idx] = true
+
+            visitors[idx].forEach {
+                if (!correct[it])queue.add(it)
+            }
+        }
+
+        var idx = 0
+        var acc = 0
+
+        val visited = MutableList(input.size) { false }
+
+        while(idx in 0 until input.size && !visited[idx]) {
+            visited[idx] = true
+            val (op, value) = input[idx].split(" ")
+
+            when (op) {
+                "acc" -> {
+                    acc += value.toInt()
+                    idx++
+                }
+                "jmp" -> {
+                    if (!correct[idx] && correct[idx+1]) idx++
+                    else idx += value.toInt()
+                }
+                "nop" -> {
+                    if (!correct[idx] && correct[idx+value.toInt()]) idx += value.toInt()
+                    else idx ++
+                }
+            }
+        }
+
+        return acc
+    }
+
     val input = readInput("Day08")
     part1(input).println()
     part2(input).println()
+    part2Graph(input).println()
 }
